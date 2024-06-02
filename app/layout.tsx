c792d88eff022d4c8cf6b15c4c2a2a9b55d55324
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-06-02 18:01:10 +0530

    authentication: using clerk and connected with convex::: bhot hard tha

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ConvexClientProvider from '@/app/providers/ConvexClientProvider'
+import ConvexClerkProvider from '@/app/providers/ConvexClerkProvider'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-      </body>
-    </html>
+    <ConvexClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+        </body>
+      </html>
+    </ConvexClerkProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://social-pelican-1.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig
